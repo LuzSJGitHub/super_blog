bc1261594c8b70cccf9d1505a1ad71f54d544302
Luz2021 <33913286+LuzSJGitHub@users.noreply.github.com>
2021-11-06 17:03:14 +0000

    Tarea 17 hecho

---
+++ app/models/message.rb
@@ -0,0 +1,8 @@
+class Message < ApplicationRecord
+  belongs_to :post
+  belongs_to :user
+
+  validates :author, :message,
+     presence: {message: "Este campo es requerido"}
+
+end
---
+++ app/models/post.rb
@@ -0,0 +1,11 @@
+class Post < ApplicationRecord
+  has_many :message
+  
+  belongs_to :blog
+  belongs_to :user
+
+  validates :title,
+    :content, presence: {message: "Este campo es requerido"}
+
+end
+
